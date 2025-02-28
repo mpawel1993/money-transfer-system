f4577d95ededee806838683454a0485439b9afb4
Pawel Mazur <mpawe1993@gmail.com>
2025-02-28 19:41:54 +0100

    init keycloak and spring server

---
+++ fortuna-bank-server/src/main/java/pl/mazur/pawel/fortuna_bank_server/FortunaBankServerApplication.java
@@ -0,0 +1,13 @@
+package pl.mazur.pawel.fortuna_bank_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortunaBankServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortunaBankServerApplication.class, args);
+	}
+
+}
---
+++ fortuna-bank-server/src/test/java/pl/mazur/pawel/fortuna_bank_server/FortunaBankServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mazur.pawel.fortuna_bank_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortunaBankServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
